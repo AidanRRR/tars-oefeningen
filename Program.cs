70b8809418d4daa7553eb5c594dace2a88329917
DESKTOP-8OUDKEN\Aidan <aidan.rypens@outlook.com>
2017-07-11 09:15:39 +0200

    Project setup

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ctest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrTest = new int[] { 1, 50, 60, 80 };
+            int som = 0;
+
+            for (int i = 0; i < arrTest.Length; i++)
+            {
+                som += arrTest[i];
+            }
+
+            Console.WriteLine(som);
+        }
+    }
+}
